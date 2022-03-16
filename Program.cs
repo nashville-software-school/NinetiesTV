d6837632ceec3701092dacd9d35553245c0f2346
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-03-15 22:47:47 -0400

    2/5 bonus questions done

--- Program.cs
+++ Program.cs
@@ -123,7 +123,8 @@ namespace NinetiesTV
         static List<string> ComediesByRating(List<Show> shows)
         {
             List<Show> shws = shows.Where(s => s.Genres.Contains("Comedy")).OrderByDescending(s => s.ImdbRating).ToList();
-            return shws.Select(s => s.Name).ToList();
+            return shows.SelectMany(s => s.Genres).Distinct().ToList();
+            // return shws.Select(s => s.Name).ToList();
         }
 
         // 14. Return the shows with more than one genre ordered by their starting year.
@@ -204,10 +205,18 @@ namespace NinetiesTV
 
         **************************************************************************************************/
 
-        // 1. Return the genres of the shows that started in the 80s.
+        static List<String> b1(List<Show> shows)
+        {
+            // 1. Return the genres of the shows that started in the 80s.
 
-        shows.Where(s => s.StartsWith< 1990);
-        // 2. Print a unique list of geners.
+            return shows.Where(s => s.StartYear < 1990).ToList().SelectMany(s => s.Genres).ToList().Distinct().ToList();
+        }
+
+        // 2. Print a unique list of generes.
+        static List<String> b2(List<Show> shows)
+        {
+            return shows.SelectMany(s => s.Genres).Distinct().ToList();
+        }
         // 3. Print the years 1987 - 2018 along with the number of shows that started in each year (note many years will have zero shows)
         // 4. Assume each episode of a comedy is 22 minutes long and each episode of a show that isn't a comedy is 42 minutes. How long would it take to watch every episode of each show?
         // 5. Assume each show ran each year between its start and end years (which isn't true), which year had the highest average IMDB rating.
