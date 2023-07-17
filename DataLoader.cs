9824285b5b44df1538ed1f199470b6d244adef66
Josh Barton <joshdbarton@gmail.com>
2023-07-17 13:25:01 -0400

    updating to net 6 with top-level statements

--- DataLoader.cs
+++ DataLoader.cs
@@ -1,7 +1,4 @@
-using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
-using System.Linq;
 using CsvHelper;
 using CsvHelper.Configuration;
 
@@ -14,34 +11,32 @@ You DO NOT need to understand this code.
 
 ******************************************************************/
 
-namespace NinetiesTV
+namespace NinetiesTV;
+public class DataLoader
 {
-    public class DataLoader
+    private static string _dataFilename = "data.csv";
+    public static List<Show> GetShows()
     {
-        private static string _dataFilename = "data.csv";
-        public static List<Show> GetShows()
+        using (TextReader textReader = new StreamReader(_dataFilename))
         {
-            using(TextReader textReader = new StreamReader(_dataFilename))
+            using (CsvReader csv = new CsvReader(textReader, CultureInfo.CurrentCulture))
             {
-                using(CsvReader csv = new CsvReader(textReader, CultureInfo.CurrentCulture))
-                {
-                    csv.Context.RegisterClassMap<ShowMap>();
-                    return csv.GetRecords<Show>().ToList();
-                }
+                csv.Context.RegisterClassMap<ShowMap>();
+                return csv.GetRecords<Show>().ToList();
             }
         }
+    }
 
-        class ShowMap : ClassMap<Show>
+    class ShowMap : ClassMap<Show>
+    {
+        public ShowMap()
         {
-            public ShowMap()
-            {
-                Map(m => m.Name);
-                Map(m => m.StartYear);
-                Map(m => m.EndYear);
-                Map(m => m.Genres).Convert(arg => arg.Row.GetField("Genres").Split(";").ToList());
-                Map(m => m.EpisodeCount);
-                Map(m => m.ImdbRating);
-            }
+            Map(m => m.Name);
+            Map(m => m.StartYear);
+            Map(m => m.EndYear);
+            Map(m => m.Genres).Convert(arg => arg.Row.GetField("Genres").Split(";").ToList());
+            Map(m => m.EpisodeCount);
+            Map(m => m.ImdbRating);
         }
     }
 }
